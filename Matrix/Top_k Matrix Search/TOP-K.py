a02f53a8746e041e78f55bf5f1557896efcc10cb
jelhamm <elhamjafari200312@gmail.com>
2023-08-17 16:54:42 +0430

    Creating a matrix class

---
+++ Matrix/Top_k Matrix Search/TOP-K.py
@@ -0,0 +1,21 @@
+# This program can receive a matrix as an input and search 
+# for the top elements in the matrix with the help of the (Top-k) Algorithm.
+
+
+class Matrix:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.matrix = []
+
+    def enterElements(self):
+        for i in range(self.rows):
+            row = []
+            for j in range(self.columns):
+                element = float(input(f"==> Enter the element at position ({i+1}, {j+1}): "))
+                row.append(element)
+            self.matrix.append(row)
+    
+    def display(self):
+        for row in self.matrix:
+            print(row)
