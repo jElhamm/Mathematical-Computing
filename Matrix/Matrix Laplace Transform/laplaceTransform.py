56a845b7e00b2bce4c23c8309448d8334831de2d
jelhamm <elhamjafari200312@gmail.com>
2023-08-20 23:00:33 +0430

    Implementation of the Laplace transform function

--- Matrix/Matrix Laplace Transform/laplaceTransform.py
+++ Matrix/Matrix Laplace Transform/laplaceTransform.py
@@ -17,3 +17,14 @@ class Matrix:
     def display(self):
         for row in self.matrix:
             print(row)
+
+    # Apply the Laplace transform to each element in the matrix
+    def laplaceTransform(self):
+        s = float(input("==> Enter the value of s: "))                    # Get the value of s from the user
+        result = [[0] * self.columns for _ in range(self.rows)]           # Create an empty matrix for the result
+        
+        # result = np.zeros((self.rows, self.columns))
+        for i in range(self.rows):                                        #  Apply the Laplace transform
+            for j in range(self.columns):
+                result[i][j] = self.matrix[i][j] / (s + 1)
+        return result
