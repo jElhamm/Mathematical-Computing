bbd746049688b4ecc533576cc3386c4d0a0f7b42
jelhamm <elhamjafari200312@gmail.com>
2023-08-20 22:40:11 +0430

    Creating a matrix class

---
+++ Matrix/Matrix Laplace Transform/laplaceTransform.py
@@ -0,0 +1,19 @@
+# You can use this program to calculate the Laplace transform of numerical matrices.
+  
+class Matrix:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.matrix = []
+
+    def enterElements(self):
+        for i in range(self.rows):
+            row = []
+            for j in range(self.columns):
+                element = float(input(f"==> Enter the element at position ({i+1}, {j+1}): "))
+                row.append(element)
+            self.matrix.append(row)
+    
+    def display(self):
+        for row in self.matrix:
+            print(row)
