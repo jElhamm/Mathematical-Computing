2e16f8568e930f2812f793b90ce8a11cdae67542
jelhamm <elhamjafari200312@gmail.com>
2023-08-22 17:27:53 +0430

    Creating a matrix class

---
+++ Matrix/Fourier Transform Of Matrix (with numpy)/FourierTransform.py
@@ -0,0 +1,23 @@
+# You can use this program to calculate the Fourier transform of matrices.
+# We use the (numpy) library to perform calculations.
+
+# Fourier series method is used in this program.
+
+
+class Matrix:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.matrix = []
+
+    def enterElements(self):
+        for i in range(self.rows):
+            row = []
+            for j in range(self.columns):
+                element = float(input(f"==> Enter the element at position ({i+1}, {j+1}): "))
+                row.append(element)
+            self.matrix.append(row)
+    
+    def display(self):
+        for row in self.matrix:
+            print(row)
