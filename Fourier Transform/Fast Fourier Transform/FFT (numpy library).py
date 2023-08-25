5c6e5d7781c4b955086db73b9b86c8eb33982590
jelhamm <elhamjafari200312@gmail.com>
2023-08-25 21:14:01 +0430

    Full implementation of FFT algorithm

--- Fourier Transform/Fast Fourier Transform/FFT (numpy library).py
+++ Fourier Transform/Fast Fourier Transform/FFT (numpy library).py
@@ -19,3 +19,21 @@ class FFTCalculator:
     def print_results(self):
         for k, X_k in enumerate(self.X):
             print("X[{}] = {}".format(k, X_k))
+
+    # FFT algorithm (1)
+    def fft(self):
+        if self.N <= 1:
+            self.X = self.x
+        else:
+            even = FFTCalculator(self.N // 2)
+            odd = FFTCalculator(self.N // 2)
+            even.x = [self.x[i] for i in range(0, self.N, 2)]        # Extract even indices from input sequence
+            odd.x = [self.x[i] for i in range(1, self.N, 2)]         # Extract odd indices from input sequence
+            even.fft()
+            odd.fft()
+            
+            # Perform FFT calculation
+            for k in range(self.N // 2):
+                t = np.exp(-2j * np.pi * k / self.N) * odd.X[k]
+                self.X[k] = even.X[k] + t
+                self.X[k + self.N // 2] = even.X[k] - t
