fc1f89acfda840218cb556e322275e55d8aff75b
jelhamm <elhamjafari200312@gmail.com>
2023-08-26 19:13:26 +0430

    Display Output

--- Fourier Transform/Discrete Fourier Transform/DFT (numpy library).py
+++ Fourier Transform/Discrete Fourier Transform/DFT (numpy library).py
@@ -32,3 +32,43 @@ class DFTCalculator:
     # def calculate_DFT(self):
     #     self.X = np.fft.fft(self.x)
 
+
+# This part of the code is written as an example to show the output of the code.
+# According to your needs, you can change or delete this part.
+
+def  banner():
+    print("""
+        
+#############################################################################################
+                                 ***   Welcome   ***                                         
+                                                                                                          
+               You can use this program to calculate the Fourier transform.                  
+                                                                                             
+    ____________________________________________________________________________________          
+   | The formula used for calculation is:   X_k = Σ(x[n] * e^(-j * 2π * k * n / N)) "   |    
+   |____________________________________________________________________________________|   
+   | 1 |  X_k  --> The value of the Discrete Fourier Transform at frequency k           |    
+   |____________________________________________________________________________________|    
+   | 2 |  x[n] --> Input signal at time n                                               |    
+   |____________________________________________________________________________________|    
+   | 3 |  e    --> Euler's number                                                       |    
+   |____________________________________________________________________________________|    
+   | 4 |  e^(-j * 2π * k * n / N) --> The inverse exponential function                  |    
+   |____________________________________________________________________________________|    
+                                                                                                 
+#############################################################################################
+    """)
+
+def main():
+    banner()
+    N = int(input("==> Enter the number of samples (N): "))
+    calculator = DFTCalculator(N)
+    calculator.calculateInputValues()
+    calculator.calculate_DFT()
+    calculator.print_results()
+    print("*********************************************************************\n")
+    
+if __name__ == "__main__":
+    main()
+
+# An example of how to use the program is shown.
