69da5c95ccb22d0b42bd423937b16afda7394074
jelhamm <elhamjafari200312@gmail.com>
2023-08-29 01:49:34 +0430

    Display Output

--- Statistical Calculations/Standard Deviation/standardDeviation (math_Library).py
+++ Statistical Calculations/Standard Deviation/standardDeviation (math_Library).py
@@ -29,3 +29,41 @@ class StandardDeviation:
     def print_standardDeviation(self):
         standard_deviation = self.calculate_standardDeviation()
         print("   ---> Standard Deviation:", standard_deviation)
+
+
+# This part of the code is written as an example to show the output of the code.
+# According to your needs, you can change or delete this part.
+
+def  banner():
+    print("""
+        
+################################################################################
+#                              ***   Welcome   ***                             #
+#                                                                              #             
+#        You can use the program to calculate the (Standard Deviation).        #
+#          
+#              The formula for standard deviation is as follows:               #
+#                           σ = √(Σ(xi - μ)² / N)                              #
+#                   --->    σ = standard deviation                             #
+#                   --->    Σ = sum of                                         #
+#                   --->    xi = individual data point                         #
+#                   --->    μ = mean (average)                                 #
+#                   --->    N = number of data points                          #
+#                                                                              #    
+################################################################################
+    """)
+
+def main():
+    banner()
+    data = input("   ---> Enter a list of data (separated by spaces): ").split()
+    data = [float(x) for x in data]
+    standard_deviation_calculator = StandardDeviation(data)
+    print("\n**********************************************************************************")
+    standard_deviation_calculator.print_standardDeviation()
+    print("**********************************************************************************\n")
+
+
+if __name__ == "__main__":
+    main()
+
+# An example of how to use the program is shown.
