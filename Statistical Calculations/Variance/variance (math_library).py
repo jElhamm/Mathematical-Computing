3df9193ff0f445b0776eda22d2b6f4d214787e79
jelhamm <elhamjafari200312@gmail.com>
2023-08-29 03:00:36 +0430

    Display Output

--- Statistical Calculations/Variance/variance (math_library).py
+++ Statistical Calculations/Variance/variance (math_library).py
@@ -28,3 +28,39 @@ class Variance:
         variance = self.calculate_variance()
         print("   ---> Variance:", variance)
         
+
+# This part of the code is written as an example to show the output of the code.
+# According to your needs, you can change or delete this part.
+
+def  banner():
+    print("""
+        
+################################################################################
+#                           ***   Welcome   ***                                #
+#                                                                              #
+#        You can use the program to calculate the (variance).                  #
+#                                                                              #
+#        The formula for variance is as follows:                               #
+#                Var = ∑(xᵢ - μ)² / N                                          #
+#        --->    Var = represents variance                                     #
+#        --->    Σ = sum of                                                    #
+#        --->    xi = represents each individual value in the dataset          #
+#        --->    μ = represents the mean of the dataset                        #
+#        --->    N = represents the total number of values in the dataset      #
+#                                                                              #
+################################################################################
+    """)  
+
+def main():
+    banner()
+    data = input("   ---> Enter a list of data (separated by spaces): ").split()
+    data = [float(x) for x in data]
+    variance_calculator = Variance(data)
+    print("\n**********************************************************************************")
+    variance_calculator.print_variance()
+    print("**********************************************************************************\n")
+
+if __name__ == "__main__":
+    main()
+
+# An example of how to use the program is shown.
