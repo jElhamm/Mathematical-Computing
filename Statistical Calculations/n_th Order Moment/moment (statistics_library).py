c3dcfb04f6992c443ac367a68ef85b05d8cd7372
jelhamm <elhamjafari200312@gmail.com>
2023-08-31 18:06:22 +0430

    Constructing the (Moment) class

---
+++ Statistical Calculations/n_th Order Moment/moment (statistics_library).py
@@ -0,0 +1,25 @@
+# You can use the program to calculate the (n_th Order Moment).
+
+# n_th Order Moment formula in mathematics is written as follows:    (1/N) * Σ((xi - mean)^n)
+#                                                               --->  N is the number of data points in the dataset
+#                                                               --->  xi is the ith data point
+#                                                               --->  mean is the mean of the dataset
+#                                                               --->  n is the order of the moment
+
+
+import statistics
+
+class Moment:
+    def __init__(self, data):
+        self.data = data
+
+    # Calculations
+    def calculate_nth_order_moment(self, n):
+        mean = statistics.mean(self.data)                                    # Calculate the mean
+        moment = sum((xi - mean)**n for xi in self.data) / len(self.data)    # Calculate the moment
+        return moment
+
+    def print_moment(self, n):
+        moment = self.calculate_nth_order_moment(n)
+        print("   --->", n, "th order moment:", moment)
+ 
