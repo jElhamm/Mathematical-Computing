005fc64cf47570b25bee322c6aca856805b7caaf
jelhamm <elhamjafari200312@gmail.com>
2023-08-31 17:53:27 +0430

    Display Output

--- n_th Order Moment/moment (math_library).py
+++ n_th Order Moment/moment (math_library).py
@@ -23,3 +23,43 @@ class Moment:
         moment = self.calculate_nth_order_moment(n)
         print("   --->", n, "th order moment:", moment)
  
+
+# This part of the code is written as an example to show the output of the code.
+# According to your needs, you can change or delete this part.
+
+def banner():
+    print("""
+        
+################################################################################
+#                           ***   Welcome   ***                                #
+#                                                                              #
+#        You can use the program to calculate the (n_th Order Moment).         #
+#                _____________________________________________                 #
+#               |                                             |                #
+#               |   he formula for variance is as follows:    |                #
+#               |    (1/N) * Σ((xi - mean)^n)                 |                #
+#               |_____________________________________________|                #
+#                                                                              #
+#          --->    N    = is the number of data points in the dataset          #
+#          --->    Σ    = sum of                                               #
+#          --->    xi   = is the ith data point                                #
+#          --->    mean = is the mean of the dataset                           #
+#          --->    n    = is the order of the moment                           #
+#                                                                              #
+################################################################################
+    """)
+
+def main():
+    banner()
+    data = input("   ---> Enter a list of data (separated by spaces): ").split()
+    data = [float(x) for x in data]
+    n = int(input("   ---> Enter the order of the moment: "))
+    moment_calculator = Moment(data)
+    print("\n**********************************************************************************")
+    moment_calculator.print_moment(n)
+    print("**********************************************************************************\n")
+
+if __name__ == "__main__":
+    main()
+
+# An example of how to use the program is shown.
