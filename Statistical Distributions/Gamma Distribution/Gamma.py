0d753d6d5bfd6cfe5ecc75c63d2670c95d13d12e
jelhamm <elhamjafari200312@gmail.com>
2023-09-24 17:36:32 +0330

    Constructing the (Data_Analyzer) Class

--- Statistical Distributions/Gamma Distribution/Gamma.py
+++ Statistical Distributions/Gamma Distribution/Gamma.py
@@ -36,3 +36,16 @@ def plot(self):
     plt.grid(True)                                                      # Add a grid to the graph
     plt.show()
   
+class DataAnalyzer:
+    def __init__(self):
+        self.data = []
+
+    def read_data(self):
+        data_input = input("---> Enter a list of numbers (comma-separated): ")
+        self.data = [float(x.strip()) for x in data_input.split(',')]
+
+    def calculate_statistics(self):
+        mean = np.mean(self.data)
+        print("---> Mean:", mean)
+        return mean
+ 
