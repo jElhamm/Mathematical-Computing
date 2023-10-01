a3fc145d5d9b86074534586fe20f5d503421212e
jelhamm <elhamjafari200312@gmail.com>
2023-10-01 22:43:00 +0330

    Constructing the (Data_Analyzer) Class

--- Graph/Breadth-First Search (directed graphs)/BFS (math_igraph).py
+++ Graph/Breadth-First Search (directed graphs)/BFS (math_igraph).py
@@ -23,3 +23,35 @@ class Graph:
         except igraph._igraph.InternalError:
             return None
  
+
+# The DataAnalysis class utilizes the Graph class to analyze data by taking user input for the number of edges and
+# the edges themselves, and then finding the shortest path between a start node and a goal node.
+
+class DataAnalysis:
+    def __init__(self):
+        self.graph = Graph()
+
+    def print_graph_shape(self):
+        print(" *** Graph Shape: ***")
+        for node in self.graph.graph.vs:
+            neighbors = self.graph.graph.neighbors(node.index)
+            print(f" {node['name']} -> {', '.join(self.graph.graph.vs[neighbors]['name'])}")
+
+    def analyze_data(self):
+        n = int(input("---> Enter the number of edges: "))
+        for _ in range(n):
+            u, v = input("---> Enter an edge (space-separated): ").split()
+            self.graph.add_edge(u, v)
+
+        start_node = input("---> Enter the start node: ")
+        goal_node = input("---> Enter the goal node: ")
+        print("***************************************************************************\n")
+
+        self.print_graph_shape()
+
+        shortest_path = self.graph.bfs(start_node, goal_node)
+        if shortest_path:
+            print("\n *** Shortest path:", "->".join(self.graph.graph.vs[shortest_path]['name']))
+        else:
+            print(" (  !  No path found.  !  )")
+ 
