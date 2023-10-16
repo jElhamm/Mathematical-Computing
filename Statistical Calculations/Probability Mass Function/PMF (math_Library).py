da3a78b7d2a19df8e325ca84c941ac0f990ae608
jelhamm <elhamjafari200312@gmail.com>
2023-10-16 21:20:30 +0330

    Display Output

--- Statistical Calculations/Probability Mass Function/PMF (math_Library).py
+++ Statistical Calculations/Probability Mass Function/PMF (math_Library).py
@@ -27,3 +27,36 @@ class ProbabilityMassFunction:
         for value, prob in pmf.items():
             print(f"      - P({value}) = {prob}")
  
+
+
+# This part of the code is written as an example to show the output of the code.
+# According to your needs, you can change or delete this part.
+
+def banner():
+    print("""
+        
+################################################################################
+#                              ***   Welcome   ***                             #
+#                                                                              #             
+#      You can use the program to calculate the Probability Mass Function.     #
+#                                                                              #
+#            The Probability Mass Function (PMF) calculates the                #
+#          probability of each value occurring in the given data set.          #
+#                                                                              #    
+################################################################################
+    """)
+
+def main():
+    banner()
+    data = input("   ---> Enter a list of data (separated by spaces): ").split()
+    data = [float(x) for x in data]
+    pmf_calculator = ProbabilityMassFunction(data)
+    print("\n**********************************************************************************")
+    pmf_calculator.print_pmf()
+    print("**********************************************************************************\n")
+
+if __name__ == "__main__":
+    main()
+
+
+# An example of how to use the program is shown. 
